52f2ff8ef392dd35f54986dd46ade80e97c577d2
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2020-03-10 20:47:30 +0800

    Add a clone method to Clause class

--- src/clause.py
+++ src/clause.py
@@ -1,5 +1,9 @@
+from copy import deepcopy
+
 class Clause:
 	def __init__(self, id, literals):
 		self.id = id
 		self.literals = literals
 
+	def clone(self):
+		return Clause(self.id, deepcopy(self.literals))
