37bd5c8b53ab7a35fcad8cab1477c62c29d6a240
LeowWB <41581512+LeowWB@users.noreply.github.com>
2020-05-08 20:28:17 +0800

    Refactor

--- src/cdcl.py
+++ src/cdcl.py
@@ -23,7 +23,7 @@ class Cdcl:
 		self.MAX_ID = len(F)
 		self.flat = flat	# whether we'll flatten output
 		self.L = []			# lemmas that we've learnt
-		self.decisions = [] # decisions we've made that aren't in declist.
+		self.decisions = [] # decisions we've made that aren't in dec_list.
 		self.num_vars = len(ap_formula(F))
 		self.pure_lit_timer = 0 # every so often, we will eradicate all pure literals
 
@@ -40,81 +40,62 @@ class Cdcl:
 	# lit_list is a list of literals that we want to unit-propagate. they will be treated with highest priority.
 	def unit_prop(self, F, level, G, lit_list = None):
 		assert is_formula(F), "unit_prop assert formula" + str(F)
-		propList = [] # vars assigned thru inference
+		prop_list = [] # vars assigned thru inference
 		agenda = [] #stack. purpose of this is so we explore in a more DFS-like manner
 
 		while (lit_list or (exists_unit_clause(F) and not contains_empty_clause(F))):
 			if lit_list:
-				unitClause = make_singleton_clause(lit_list.pop())
+				unit_clause = make_singleton_clause(lit_list.pop())
 			elif (len(agenda) == 0):
-				unitClause = find_unit_clause(F)
+				unit_clause = find_unit_clause(F)
 			else:
-				unitClause = agenda.pop()
-				if not len(unitClause) == 1:
+				unit_clause = agenda.pop()
+				if not len(unit_clause) == 1:
 					continue
-			l = unpack_unit_clause(unitClause)
-			propList.append(l)
+			l = unpack_unit_clause(unit_clause)
+			prop_list.append(l)
 			F = self.resolve(l, F, agenda, level)
-			G = self.update_graph(G, propList, unitClause, level)
-		return (propList, F, G)
+			G = self.update_graph(G, prop_list, unit_clause, level)
+		return (prop_list, F, G)
 
-	# fit_in - true if this cdcl iter continues another level (rather than starting a new lvl). affects how propList is added to decList.
+	# fit_in - true if this cdcl iter continues another level (rather than starting a new lvl). affects how prop_list is added to dec_list.
 	# next_prop is a list of literals we want to unit prop the next cycle.
-	def cdcl(self, F, decList, level, G, fit_in = False, next_prop = None):
-		(propList, F, G) = self.unit_prop(F, level, G, lit_list = next_prop)
+	def cdcl(self, F, dec_list, level, G, fit_in = False, next_prop = None):
+		(prop_list, F, G) = self.unit_prop(F, level, G, lit_list = next_prop)
 		# if level is 0, we can't backtrack any further. so any inferences we make, we can apply to the actual self.F.
 		if level == 0:
 			self.F = copy.deepcopy(F)
-			self.decisions += propList
-			propList = []
+			self.decisions += prop_list
+			prop_list = []
 
 		if not fit_in:
-			decList.append(propList)
+			dec_list.append(prop_list)
 		else:
-			decList[-1] = decList[-1] + propList
+			dec_list[-1] = dec_list[-1] + prop_list
 
 		if contains_empty_clause(F):
 			if level==0:
 				return (UNSAT, None, 0) # empty clause at level 0 implies we've derived two directly contradictory clauses thru resolution
 
 			empty_clause = find_empty_clause(F)
-			newLemma = self.diagnose(G, decList, empty_clause.id, F)
-			self.F.add_clause(copy.deepcopy(newLemma))
+			new_lemma = self.diagnose(G, dec_list, empty_clause.id, F)
+			self.F.add_clause(copy.deepcopy(new_lemma))
 
-			# updates the newLemma to the current state of decisions
-			F.add_clause(newLemma)
-			for i in range(len(decList)):
-				sublist = decList[i]
-				for sublit in sublist:
-					if lnot(sublit) in newLemma.all_literals():
-						newLemma.remove_literal(lnot(sublit), i)
-
-			# if the new clause is a unit clause, backtrack all the way to the start
-			if len(newLemma)==1:
-				return (UNSAT, None, 1)
-
-			# else, we map each var in the new clause to the level it was assigned, and go to the second-most recent of those levels
-			# rationale: at that point, all but one of the vars will have been assigned. we can then infer the last var.
-			biggest = [0,0]
-			for clauseVar in ap_clause(newLemma):
-				curVarLevel = G.nodes[int(clauseVar)]["l"]
-				if curVarLevel > biggest[0]:
-					biggest[1] = biggest[0]
-					biggest[0] = curVarLevel
-				elif curVarLevel > biggest[1]:
-					biggest[1] = curVarLevel
-			return (UNSAT, None, biggest[1])
+			# updates the new_lemma to the current state of decisions
+			F.add_clause(new_lemma)
+			self.update_clause(new_lemma, dec_list)
+			return (UNSAT, None, self.decide_backjump_level(new_lemma))
 		if (is_empty_cnf(F)):
-			return (SAT, decList)
+			return (SAT, dec_list)
 		l = self.select_literal(F)
-		result1 = self.cdcl(F, decList, level+1, G, next_prop = [l])
+		result1 = self.cdcl(F, dec_list, level+1, G, next_prop = [l])
 		
 		if result1[0] == SAT:
 			return result1
 
 		# since the above if-statement didn't get called, we assume the result was UNSAT.
-		# so we undo the effects of the previous guess, from both G and decList
-		G.remove_nodes_from([int(ap_literal(x)) for x in decList.pop()])
+		# so we undo the effects of the previous guess, from both G and dec_list
+		G.remove_nodes_from([int(ap_literal(x)) for x in dec_list.pop()])
 		
 		# this if-block handles backjumping. if the conflict diagnosis told us to backjump further
 		# back than this level, then we just do so by returning.
@@ -130,7 +111,7 @@ class Cdcl:
 			self.pure_lit_timer = 0
 			F, pure_lits = self.eradicate_pure_lits(F)
 
-		return self.cdcl(F, decList, level, G, True, pure_lits)
+		return self.cdcl(F, dec_list, level, G, True, pure_lits)
 
 	# note that l is a literal, not prop var.
 	def resolve(self, l, F, agenda, level):
@@ -167,10 +148,10 @@ class Cdcl:
 		return max_lit
 
 	# this is called after unit propagation resolution. it updates the inference graph.
-	def update_graph(self, G, propList, unitClause, level):
-		literal = unpack_unit_clause(unitClause) # the literal that we just decided
+	def update_graph(self, G, prop_list, unit_clause, level):
+		literal = unpack_unit_clause(unit_clause) # the literal that we just decided
 		propVar = ap_literal(literal)	# the propvar whose value we have just decided
-		if unitClause.id == -1:
+		if unit_clause.id == -1:
 			# unit clause was created as the result of a guess
 			assert not (int(propVar) in set(G.nodes)), "why's it inside"
 			G.add_node(int(propVar))
@@ -178,14 +159,14 @@ class Cdcl:
 			G.nodes[int(propVar)]["l"] = level
 		else:
 			# unit clause was resolved from a clause that was present in the original F
-			assert unitClause.id >= 0
+			assert unit_clause.id >= 0
 			G.add_node(int(propVar))
 			G.nodes[int(propVar)]["v"] = not is_neg_literal(literal)
 			G.nodes[int(propVar)]["l"] = level
-			G.nodes[int(propVar)]["reason"] = unitClause.id
+			G.nodes[int(propVar)]["reason"] = unit_clause.id
 
 			# loop thru the literals of the original clause, other than the one that became part of the unit clause
-			for original_clause_lit in self.F._clauses[unitClause.id].all_literals():
+			for original_clause_lit in self.F._clauses[unit_clause.id].all_literals():
 				if original_clause_lit == literal:
 					continue
 				parent_node_id = int(ap_literal(original_clause_lit))
@@ -194,8 +175,8 @@ class Cdcl:
 		return G
 
 	# to avoid confusion all vars are stored as strings (just like in the clauses), unless in the graph.
-	def diagnose(self, G, decList, emptyClauseId, formula):
-		curLevelVars = list(map(ap_literal, decList[-1]))	# all vars assigned in current level
+	def diagnose(self, G, dec_list, emptyClauseId, formula):
+		curLevelVars = list(map(ap_literal, dec_list[-1]))	# all vars assigned in current level
 		theRandomlyAssignedVar = None # in the current lvl, the ONE var that was randomly assigned (can we just assume to be first in curLevelVars?) TODO
 		for v in curLevelVars:
 			v = int(v)
@@ -255,6 +236,30 @@ class Cdcl:
 				pure_lit_decisions.append(str(i+1))
 		
 		return F, pure_lit_decisions
+
+	# forcefully updates a clause to the current state of affairs (according to dec_list)
+	def update_clause(self, clause, dec_list):
+		for i in range(len(dec_list)):
+			sublist = dec_list[i]
+			for sublit in sublist:
+				if lnot(sublit) in clause.all_literals():
+					clause.remove_literal(lnot(sublit), i)
+
+	def decide_backjump_level(self, new_lemma):
+		# if the new clause is a unit clause, backtrack all the way to the start
+		if len(new_lemma)==1:
+			return (UNSAT, None, 1)
+		# else, we map each var in the new clause to the level it was assigned, and go to the second-most recent of those levels
+		# rationale: at that point, all but one of the vars will have been assigned. we can then infer the last var.
+		biggest = [0,0]
+		for clause_var in ap_clause(new_lemma):
+			cur_var_level = G.nodes[int(clause_var)]["l"]
+			if cur_var_level > biggest[0]:
+				biggest[1] = biggest[0]
+				biggest[0] = cur_var_level
+			elif cur_var_level > biggest[1]:
+				biggest[1] = cur_var_level
+		return biggest[1]
 
 # TODO re3move this
 def deb_print(x):
